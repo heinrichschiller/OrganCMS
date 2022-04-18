1ac2ef952c6d4392a0ef4c21fb7a4f66d7bae1ba
heinrichschiller <heinrich@heinrich-schiller.de>
2022-04-18 08:28:17 +0200

    added, organ register stuff

---
+++ tests/Domain/Donation/RegisterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Domain\Donation\Register;
+use PHPUnit\Framework\TestCase;
+
+class RegisterClass extends TestCase
+{
+    /** @covers App\Domain\Donation\Register */
+    public function testRegisterTest(): void
+    {
+        $register = new Register(1, 'name');
+
+        $this->assertInstanceOf(Register::class, $register);
+    }
+}

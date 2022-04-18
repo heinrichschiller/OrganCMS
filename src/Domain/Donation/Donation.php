a67981e349810c42ce4b2bd45925966ce9800172
heinrichschiller <heinrich@heinrich-schiller.de>
2022-04-18 08:30:43 +0200

    phpdoc comments and refactoring

--- src/Domain/Donation/Donation.php
+++ src/Domain/Donation/Donation.php
@@ -4,86 +4,378 @@ declare(strict_types=1);
 
 namespace App\Domain\Donation;
 
-final class Donation
+class Donation
 {
     /**
-     * @var string
+     * @var bool
      */
-    private string $total='';
+    private bool   $isOneTimeDonation = false;
+
+    /**
+     * @var float
+     */
+    private float  $oneTimeDonationSum = 0.0;
+
+    /**
+     * @var bool
+     */
+    private bool   $isSponsorship = false;
+
+    /**
+     * @var float
+     */
+    private float  $sponsorshipSum = 0.0;
 
     /**
      * @var string
      */
-    private string $date = '';
+    private string $wishPipe = '';
 
     /**
      * @var string
      */
-    private string $user = '';
+    private string $whistleSound = '';
 
     /**
-     * Get total sum
-     *
+     * @var string
+     */
+    private string $whistleRegister = '';
+
+    /**
+     * @var string
+     */
+    private string $whistleWork = '';
+
+    /**
+     * @var bool
+     */
+    private bool   $isDonorWish = false;
+
+    /**
+     * @var bool
+     */
+    private bool   $isAlternativeForDonor = false;
+
+    /**
+     * @var bool
+     */
+    private bool   $isGift = false;
+
+    /**
+     * @var string
+     */
+    private string $nameOfGiftRecipient = '';
+
+    /**
+     * @var bool
+     */
+    private bool   $isBankTransfer = false;
+
+    /**
+     * @var bool
+     */
+    private bool   $isDonationReceipt = false;
+
+    /**
+     * @var bool
+     */
+    private bool   $isAgree = false;
+
+    /**
+     * Get one time donation
+     * 
+     * @return bool
+     */
+    public function getOneTimeDonation(): bool
+    {
+        return $this->isOneTimeDonation;
+    }
+
+    /**
+     * Set one time donation
+     * 
+     * @param bool $isOneTimeDonation
+     */
+    public function setOneTimeDonation(bool $isOneTimeDonation): void
+    {
+        $this->isOneTimeDonation = $isOneTimeDonation;
+    }
+
+    /**
+     * Get one time donation sum
+     * 
+     * @return bool
+     */
+    public function getOneTimeDonationSum(): float
+    {
+        return $this->oneTimeDonationSum;
+    }
+
+    /**
+     * Set one time donation sum
+     * 
+     * @return bool
+     */
+    public function setOneTimeDonationSum(float $sum): void
+    {
+        $this->oneTimeDonationSum = $sum;
+    }
+
+    /**
+     * Get sponsorship
+     * 
+     * @return bool
+     */
+    public function getSponsorship(): bool
+    {
+        return $this->isSponsorship;
+    }
+
+    /**
+     * Set sponsorship
+     * 
+     * @param bool
+     */
+    public function setSponsorship(bool $isSponsorship):void
+    {
+        $this->isSponsorship = $isSponsorship;
+    }
+
+    /**
+     * Get sponsorship sum
+     * 
+     * @return float
+     */
+    public function getSponsorshipSum(): float
+    {
+        return $this->sponsorshipSum;
+    }
+
+    /**
+     * Set sponsorship sum
+     * 
+     * @param float $sponsorshipSum
+     */
+    public function setSponsorshipSum(float $sponsorshipSum): void
+    {
+        $this->sponsorshipSum = $sponsorshipSum;
+    }
+
+    /**
+     * Get wish pipe
+     * 
      * @return string
      */
-    public function getTotal(): string
+    public function getWishPipe(): string
     {
-        return $this->total;
+        return $this->wishPipe;
     }
 
     /**
-     * Set total sum
-     *
-     * @param string $total
+     * Set wish pipe
+     * 
+     * @param string $wishPipe
      */
-    public function setTotal(string $total): void
+    public function setWishPipe(string $wishPipe): void
     {
-        $total = trim($total, " \n\r\t\v\0");
-
-        $this->total = $total;
+        $this->wishPipe = $wishPipe;
     }
 
     /**
-     * Get date
-     *
+     * Get whistle sound
+     * 
      * @return string
      */
-    public function getDate(): string
+    public function getWhistleSound(): string
     {
-        return $this->date;
+        return $this->whistleSound;
     }
 
     /**
-     * Set date
-     *
-     * @param string $date
+     * Set whistle sound
+     * 
+     * @param string $whistleSound
      */
-    public function setDate(string $date): void
+    public function setWhistleSound(string $whistleSound): void
     {
-        $date = trim($date, " \n\r\t\v\0");
-        
-        $this->date = $date;
+        $this->whistleSound = $whistleSound;
     }
 
     /**
-     * Get user
-     *
-     * @return string $user
+     * Get whistle register
+     * 
+     * @return string
      */
-    public function getUser(): string
+    public function getWhistleRegister(): string
     {
-        return $this->user;
+        return $this->whistleRegister;
     }
 
     /**
-     * Set user
-     *
-     * @param string $user
+     * Set whistle register
+     * 
+     * @param string
      */
-    public function setUser(string $user): void
+    public function setWhistleRegister(string $whistleRegister): void
     {
-        $user = trim($user, " \n\r\t\v\0");
+        $this->whistleRegister = $whistleRegister;
+    }
 
-        $this->user = $user;
+    /**
+     * Get whistle word
+     * 
+     * @return string
+     */
+    public function getWhistleWork(): string
+    {
+        return $this->whistleWork;
+    }
+
+    /**
+     * Set whistle work
+     * 
+     * @param string $whistleWork
+     */
+    public function setWhistleWork(string $whistleWork): void
+    {
+        $this->whistleWork = $whistleWork;
+    }
+
+    /**
+     * Is a donor wish pipe
+     * 
+     * @return bool
+     */
+    public function isDonorWish(): bool
+    {
+        return $this->isDonorWish;
+    }
+
+    /**
+     * Set donor wish
+     * 
+     * @param bool $isDonorWish
+     */
+    public function setDonorWish(bool $isDonorWish): void
+    {
+        $this->isDonorWish = $isDonorWish;
+    }
+
+    /**
+     * Is an alternative for donor
+     * 
+     * @return bool
+     */
+    public function isAlternativeForDonor(): bool
+    {
+        return $this->isAlternativeForDonor;
+    }
+
+    /**
+     * Set alternative for donor
+     * 
+     * @param bool $isAlternativeForDonor
+     */
+    public function setAlternativeForDonor(bool $isAlternativeForDonor): void
+    {
+        $this->isAlternativeForDonor = $isAlternativeForDonor;
+    }
+
+    /**
+     * Is a gift
+     * 
+     * @return bool
+     */
+    public function isGift(): bool
+    {
+        return $this->isGift;
+    }
+
+    /**
+     * Set gift
+     * 
+     * @param bool $isGift
+     */
+    public function setGift(bool $isGift): void
+    {
+        $this->isGift = $isGift;
+    }
+
+    /**
+     * Get name of the gift recipient
+     * 
+     * @return string
+     */
+    public function getNameOfGiftRecipient(): string
+    {
+        return $this->nameOfGiftRecipient;
+    }
+
+    /**
+     * Set name of the gift recipient
+     * 
+     * @param string $nameOfGiftRecipient
+     */
+    public function setNameOfGiftRecipient(string $nameOfGiftRecipient): void
+    {
+        $this->nameOfGiftRecipient = $nameOfGiftRecipient;
+    }
+
+    /**
+     * Is a bank transfer
+     */
+    public function isBankTransfer(): bool
+    {
+        return $this->isBankTransfer;
+    }
+
+    /**
+     * Set bank transfer
+     * 
+     * @param bool $isBankTransfer
+     */
+    public function setBankTransfer(bool $isBankTransfer): void
+    {
+        $this->isBankTransfer = $isBankTransfer;
+    }
+
+    /**
+     * Is a donation receipt
+     * 
+     * @var bool
+     */
+    public function isDonationReceipt(): bool
+    {
+        return $this->isDonationReceipt;
+    }
+
+    /**
+     * Set donation receipt
+     * 
+     * @param bool $isDonationReceipt
+     */
+    public function setDonationReceipt(bool $isDonationReceipt): void
+    {
+        $this->isDonationReceipt = $isDonationReceipt;
+    }
+
+    /**
+     * Donor agrees with the donation
+     * 
+     * @return bool
+     */
+    public function isAgree(): bool
+    {
+        return $this->isAgree;
+    }
+
+    /**
+     * Set Agree
+     * 
+     * @param bool $isAgree
+     */
+    public function setAgree(bool $isAgree): void
+    {
+        $this->isAgree = $isAgree;
     }
 }

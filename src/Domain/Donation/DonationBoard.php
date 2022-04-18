694aaccc73136e91c49ee2abc643e3121832aba2
heinrichschiller <heinrich@heinrich-schiller.de>
2022-04-18 08:31:47 +0200

    new, donation board

---
+++ src/Domain/Donation/DonationBoard.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Donation;
+
+final class DonationBoard
+{
+    /**
+     * @var string
+     */
+    private string $total='';
+
+    /**
+     * @var string
+     */
+    private string $date = '';
+
+    /**
+     * @var string
+     */
+    private string $user = '';
+
+    /**
+     * Get total sum
+     *
+     * @return string
+     */
+    public function getTotal(): string
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set total sum
+     *
+     * @param string $total
+     */
+    public function setTotal(string $total): void
+    {
+        $total = trim($total, " \n\r\t\v\0");
+
+        $this->total = $total;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string
+     */
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     */
+    public function setDate(string $date): void
+    {
+        $date = trim($date, " \n\r\t\v\0");
+        
+        $this->date = $date;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string $user
+     */
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     */
+    public function setUser(string $user): void
+    {
+        $user = trim($user, " \n\r\t\v\0");
+
+        $this->user = $user;
+    }
+}

5920cdf96d6cfef71b337260484d9496bdd5d791
heinrichschiller <heinrich@heinrich-schiller.de>
2022-04-18 08:22:51 +0200

    added, session message

--- src/Actions/Donation/DonationAction.php
+++ src/Actions/Donation/DonationAction.php
@@ -30,6 +30,7 @@ declare(strict_types=1);
 namespace App\Actions\Donation;
 
 use App\Domain\Donation\Service\DonationBoardReader;
+use Odan\Session\SessionInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\ViewInterface;
@@ -38,9 +39,15 @@ final class DonationAction
 {
     /**
      * @Injection
-     * @var DonationReader
+     * @var DonationBoardReader
      */
     private DonationBoardReader $reader;
+
+    /**
+     * @Injection
+     * @var SessionInterface
+     */
+    private SessionInterface $session;
 
     /**
      * @Injection
@@ -50,31 +57,49 @@ final class DonationAction
 
     /**
      * The constructor
-     * 
+     *
      * @param DonationBoardReader $reader
+     * @param SessionInterface $session
      * @param ViewInterface $view
      */
-    public function __construct(DonationBoardReader $reader, ViewInterface $view)
-    {
+    public function __construct(
+        DonationBoardReader $reader,
+        SessionInterface $session,
+        ViewInterface $view
+    ) {
         $this->reader = $reader;
+        $this->session = $session;
         $this->view = $view;
     }
 
     /**
      * The invoker
-     * 
+     *
      * @param Request $request
      * @param Response $response
      * @param array<mixed> $args
-     * 
+     *
      * @return Response
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
+        $isSuccess = false;
+        $message = '';
+
         $donation = $this->reader->read();
 
+        $flash = $this->session->getFlash();
+
+        if($flash->has('success')) {
+            $isSuccess = true;
+            $message = $flash->get('success')[0];
+        }
+        $flash->clear();
+        
         $data = [
-            'donation' => $donation
+            'donation' => $donation,
+            'isSuccess' => $isSuccess,
+            'message' => $message
         ];
 
         $response = $this->view->render($response, 'donation-board', $data);

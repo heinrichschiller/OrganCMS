96dd56c9b7e1b6ff93514223337eee16d7f416ec
Heinrich Schiller <heinrich@heinrich-schiller.de>
2022-04-28 22:11:56 +0200

    uncomment donationform scripts

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,10 @@
 import 'bootstrap'
 import SimpleLightBox from 'simplelightbox'
-import DonationForm from './DonationForm';
+// import DonationForm from './DonationForm';
 
 let gallery = new SimpleLightBox('.gallery a')
 gallery.on('show.simplelightbox', function() {
     
 });
 
-DonationForm.run()
+// DonationForm.run()

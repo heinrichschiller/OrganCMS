cb52e7adb1ccc0c1a0129f8ef15c68a10bff086b
heinrichschiller <heinrich@heinrich-schiller.de>
2022-05-25 09:24:29 +0200

    added, click events for tables and tinymce conf

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap'
 import SimpleLightBox from 'simplelightbox'
+import tinymce from 'tinymce';
 // import DonationForm from './DonationForm';
 
 let gallery = new SimpleLightBox('.gallery a')
@@ -8,3 +9,23 @@ gallery.on('show.simplelightbox', function() {
 });
 
 // DonationForm.run()
+
+tinymce.init({
+    selector: 'textarea#default-editor',
+    menubar: false,
+    toolbar: 'undo redo | bold italic underline strikethrough | fontsize  fontname | alignleft aligncenter alignright alignjustify'
+})
+
+let clickableRows = document.querySelectorAll('.clickable-row')
+
+let i = 0
+clickableRows.forEach(elm => {
+    clickableRows[i].addEventListener('click', () => {
+        let href = elm.getAttribute('data-href')
+
+        if (href) {
+            window.location = href
+        }
+    })
+    i++
+})

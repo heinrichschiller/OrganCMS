eddde5e2969493184d188f6d1789e48d90fb7f1b
heinrichschiller <heinrich@heinrich-schiller.de>
2022-05-25 09:43:30 +0200

    code refactoring and new event routes

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,22 @@ use Slim\Routing\RouteCollectorProxy;
  */
 return function(App $app)
 {
+    $app->group('/donation', function(RouteCollectorProxy $group) {
+        $group->get('/', \App\Actions\Donation\DonationBoardAction::class)->setName('donation');
+        $group->post('/update', \App\Actions\Donation\DonationUpdateAction::class)->setName('donation');
+    })->add(UserAuthMiddleware::class);
+
+    $app->group('/events', function(RouteCollectorProxy $group) {
+        $group->get('/', \App\Actions\Events\IndexAction::class)->setName('events');
+        $group->get('/new', \App\Actions\Events\NewEventAction::class)->setName('events');
+        $group->post('/create', \App\Actions\Events\CreateAction::class);
+    })->add(UserAuthMiddleware::class);
+
     $app->group('/users', function(RouteCollectorProxy $group) {
         $group->get('/', \App\Actions\User\UserAction::class)->setName('users');
         $group->get('/user', \App\Actions\User\AboutAction::class)->setName('users');
         $group->post('/user/update', \App\Actions\User\UserUpdateAction::class)->setName('users');
         $group->get('/logout', \App\Actions\Auth\LogoutAction::class)->setName('logout');
-
-        $group->get('/donation', \App\Actions\Donation\DonationBoardAction::class)->setName('donation');
-        $group->post('/donation/update', \App\Actions\Donation\DonationUpdateAction::class)->setName('users');
-
     })->add(UserAuthMiddleware::class);
 
     $app->get('/', \App\Actions\Pages\IndexAction::class);
@@ -35,6 +42,6 @@ return function(App $app)
 
     // $app->post('/send', \App\Actions\Donation\CreateAction::class);
 
-    $app->get('/veranstaltungen.html', \App\Actions\Events\EventAction::class);
+    $app->get('/veranstaltungen.html', \App\Actions\Events\PublicEventAction::class);
     $app->get('/{page}', \App\Actions\Pages\PagesAction::class);    
 };

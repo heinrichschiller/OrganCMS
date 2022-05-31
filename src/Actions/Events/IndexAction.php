a14818fd5662829b1889833181e72c8d3fea0a6e
heinrichschiller <heinrich@heinrich-schiller.de>
2022-05-31 06:57:50 +0200

    added session

--- src/Actions/Events/IndexAction.php
+++ src/Actions/Events/IndexAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Actions\Events;
 
 use App\Domain\Event\Service\EventFinder;
+use Odan\Session\SessionInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\ViewInterface;
@@ -19,6 +20,12 @@ final class IndexAction
 
     /**
      * @Injection
+     * @var SessionInterface
+     */
+    private SessionInterface $session;
+
+    /**
+     * @Injection
      * @var ViewInterface
      */
     private ViewInterface $view;
@@ -26,12 +33,18 @@ final class IndexAction
     /**
      * The constructor.
      * 
-     * @param EventFinder $finder   EventFinder service
-     * @param ViewInterface $view   Mustache template engine
+     * @param EventFinder $finder
+     * @param SessionInterface $session 
+     * @param ViewInterface $view
      */
-    public function __construct(EventFinder $finder, ViewInterface $view)
+    public function __construct(
+        EventFinder $finder,
+        SessionInterface $session,
+        ViewInterface $view
+    )
     {
         $this->finder = $finder;
+        $this->session = $session;
         $this->view = $view;
     }
 
@@ -46,10 +59,24 @@ final class IndexAction
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
+        $isSuccess = false;
+        $message = '';
+
+        $flash = $this->session->getFlash();
+
+        if($flash->has('success')) {
+            $isSuccess = true;
+            $message = $flash->get('success')[0];
+        }
+
+        $flash->clear();
+
         $events = $this->finder->findAll();
 
         $data = [
-            'events' => $events
+            'events' => $events,
+            'isSuccess' => $isSuccess,
+            'message' => $message
         ];
 
         $response = $this->view->render($response, 'event/index', $data);

e3b7a81327ea73f4c8253773a3f45b3232a434df
heinrichschiller <heinrich@heinrich-schiller.de>
2022-06-03 17:14:51 +0200

    new event routes

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ return function(App $app)
     $app->group('/events', function(RouteCollectorProxy $group) {
         $group->get('/', \App\Actions\Events\IndexAction::class)->setName('events');
         $group->get('/new', \App\Actions\Events\NewEventAction::class)->setName('events');
-        $group->get('/update/{id}', \App\Actions\Events\ReadAction::class);
+        $group->get('/update/{id}', \App\Actions\Events\ReadAction::class)->setName('read-event');
         $group->post('/create', \App\Actions\Events\CreateAction::class);
         $group->post('/update', \App\Actions\Events\UpdateAction::class);
     })->add(UserAuthMiddleware::class);

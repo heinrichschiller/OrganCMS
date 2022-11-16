37477622e20bb381a023d681d4e7299490b124d7
heinrichschiller <heinrich@heinrich-schiller.de>
2022-11-16 11:09:07 +0100

    renamed from News to Post

--- src/Actions/Pages/IndexAction.php
+++ src/Actions/Pages/IndexAction.php
@@ -6,6 +6,7 @@ namespace App\Actions\Pages;
 
 use App\Domain\Donation\DonationBoard;
 use App\Domain\Donation\Service\DonationBoardReader;
+use App\Domain\Post\Service\PostFinder;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\ViewInterface;
@@ -26,6 +27,12 @@ final class IndexAction
 
     /**
      * @Injection
+     * @var PostFinder
+     */
+    private PostFinder $postFinder;
+
+    /**
+     * @Injection
      * @var ViewInterface
      */
     private ViewInterface $view;
@@ -35,12 +42,18 @@ final class IndexAction
      *
      * @param DonationBoard $donation
      * @param DonationBoardReader $reader
+     * @param PostFinder $postFinder
      * @param ViewInterface $view
      */
-    public function __construct(DonationBoard $donation, DonationBoardReader $reader, ViewInterface $view)
-    {
+    public function __construct(
+        DonationBoard $donation,
+        DonationBoardReader $reader,
+        PostFinder $postFinder,
+        ViewInterface $view
+    ) {
         $this->donation = $donation;
         $this->reader = $reader;
+        $this->postFinder = $postFinder;
         $this->view = $view;
     }
 
@@ -56,9 +69,11 @@ final class IndexAction
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
         $this->donation = $this->reader->read();
+        $post = $this->postFinder->findMainpagePost();
 
         $data = [
-            'donation' => $this->donation
+            'donation' => $this->donation,
+            'post'     => $post
         ];
 
         $response = $this->view->render($response, 'index', $data);

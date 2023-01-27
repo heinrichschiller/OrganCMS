cc76d39946397aab64fddf09c58d88b39f0fa67d
heinrichschiller <heinrich@heinrich-schiller.de>
2023-01-27 13:39:22 +0100

    code rewrite

--- src/Actions/Pages/IndexAction.php
+++ src/Actions/Pages/IndexAction.php
@@ -4,9 +4,7 @@ declare( strict_types = 1 );
 
 namespace App\Actions\Pages;
 
-use App\Domain\Donation\DonationBoard;
-use App\Domain\Donation\Service\DonationBoardReader;
-use App\Domain\Post\Service\PostFinder;
+use App\Domain\Page\Service\IndexReader;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\ViewInterface;
@@ -15,21 +13,9 @@ final class IndexAction
 {
     /**
      * @Injection
-     * @var DonationBoard
+     * @var IndexReader
      */
-    private DonationBoard $donation;
-
-    /**
-     * @Injection
-     * @var DonationBoardReader
-     */
-    private DonationBoardReader $reader;
-
-    /**
-     * @Injection
-     * @var PostFinder
-     */
-    private PostFinder $postFinder;
+    private IndexReader $reader;
 
     /**
      * @Injection
@@ -46,35 +32,25 @@ final class IndexAction
      * @param ViewInterface $view
      */
     public function __construct(
-        DonationBoard $donation,
-        DonationBoardReader $reader,
-        PostFinder $postFinder,
+        IndexReader $reader,
         ViewInterface $view
     ) {
-        $this->donation = $donation;
         $this->reader = $reader;
-        $this->postFinder = $postFinder;
         $this->view = $view;
     }
 
     /**
      * The invoker
      *
-     * @param Request $request
-     * @param Response $response
-     * @param array<mixed> $args
+     * @param Request $request Representation of an incoming, server-side HTTP request.
+     * @param Response $response Representation of an outgoing, server-side response.
+     * @param array<string> $args Get all of the route's parsed arguments.
      *
      * @return Response
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $this->donation = $this->reader->read();
-        $post = $this->postFinder->findMainpagePost();
-
-        $data = [
-            'donation' => $this->donation,
-            'post'     => $post
-        ];
+        $data = (array) $this->reader->read();
 
         $response = $this->view->render($response, 'index', $data);
 

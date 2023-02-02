fd04967fb548b9158e41470fa63a85f0c429be12
heinrichschiller <heinrich@heinrich-schiller.de>
2023-02-02 11:03:43 +0100

    refactored, Landing page

--- src/Action/Home/HomeAction.php
+++ src/Action/Home/HomeAction.php
@@ -4,13 +4,39 @@ declare(strict_types=1);
 
 namespace App\Action\Home;
 
+use App\Domain\Home\Service\HomeReader;
+use App\Renderer\TemplateRenderer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class HomeAction
 {
     /**
-     * The invoker
+     * @Injection
+     * @var HomeReader
+     */
+    private HomeReader $reader;
+
+    /**
+     * @Injection
+     * @var TemplateRenderer
+     */
+    private TemplateRenderer $renderer;
+
+    /**
+     * The constructor.
+     *
+     * @param HomeReader $reader Home reader service
+     * @param TemplateRenderer $renderer Template renderer
+     */
+    public function __construct(HomeReader $reader, TemplateRenderer $renderer)
+    {
+        $this->reader = $reader;
+        $this->renderer = $renderer;
+    }
+
+    /**
+     * The invoker.
      *
      * @param Request $request Representation of an incoming, server-side HTTP request.
      * @param Response $response Representation of an outgoing, server-side response.
@@ -20,7 +46,9 @@ final class HomeAction
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $response->getBody()->write('Hello World!');
+        $data = $this->reader->read();
+
+        $response = $this->renderer->render($response, 'home/index', $data);
 
         return $response;
     }

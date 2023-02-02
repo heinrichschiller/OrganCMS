f50a30af7ad957cb3c90e6d64f7ce0e224c1193c
heinrichschiller <heinrich@heinrich-schiller.de>
2023-02-02 11:30:51 +0100

    added, DsgvoAction

---
+++ src/Action/Dsgvo/DsgvoAction.php
@@ -0,0 +1,44 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\Action\Dsgvo;
+
+use App\Renderer\TemplateRenderer;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+final class DsgvoAction
+{
+    /**
+     * @Injection
+     * @var TemplateRenderer
+     */
+    private TemplateRenderer $renderer;
+
+    /**
+     * The constructor.
+     *
+     * @param TemplateRenderer $renderer Template renderer
+     */
+    public function __construct(TemplateRenderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
+    /**
+     * The invoker.
+     *
+     * @param Request $request Representation of an incoming, server-side HTTP request.
+     * @param Response $response Representation of an outgoing, server-side response.
+     * @param array<string> $args Get all of the route's parsed arguments.
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, array $args = []): Response
+    {
+        $response = $this->renderer->render($response, 'dsgvo/datenschutzerklaerung.html', []);
+
+        return $response;
+    }
+}

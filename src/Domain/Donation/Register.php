8c3f648f644899a68645575453d94df4628a6b2b
heinrichschiller <heinrich@heinrich-schiller.de>
2023-02-06 21:42:33 +0100

    added, old php files

---
+++ src/Domain/Donation/Register.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Donation;
+
+final class Register
+{
+    /**
+     * The constructor
+     *
+     * @param readonly int $id
+     * @param readonly string $name Register name
+     */
+    public function __construct(
+        public readonly int $id,
+        public readonly string $name
+    ) {
+    }
+}
---
+++ src/Domain/Donation/Sound.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Donation;
+
+final class Sound
+{
+    /**
+     * The constructor
+     *
+     * @param readonly int $id
+     * @param readonly string $name
+     * @param readonly int $price
+     */
+    public function __construct(
+        public readonly int $id,
+        public readonly string $name,
+        public readonly int $price
+    ) {
+    }
+}

37710a6866af799db01ddf52dc63f00c7ceb5ccb
heinrichschiller <heinrich@heinrich-schiller.de>
2024-04-08 10:11:14 +0200

    sorted routes in frontend and backend

--- config/routes.php
+++ config/routes.php
@@ -7,64 +7,77 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/', \App\Action\Home\HomeAction::class);
-    $app->get('/blog/{id}/{slug}.html', \App\Action\Post\ReadSinglePublicPostAction::class);
-    $app->get('/datenschutzerklaerung.html', \App\Action\Dsgvo\DsgvoAction::class);
-    $app->get('/gallerie.html', \App\Action\Gallery\GalleryAction::class);
-    $app->get('/impressum.html', \App\Action\Imprint\ImprintAction::class);
-    $app->get('/blog.html', \App\Action\Post\PublicPostAction::class);
-    $app->get('/orgelakte.html', \App\Action\OrganAct\OrganActAction::class);
-    $app->get('/orgelfreunde.html', \App\Action\OrganFriend\OrganFriendAction::class);
-    $app->get('/unsere-orgel.html', \App\Action\OurOrgan\OurOrganAction::class);
-    $app->get('/veranstaltungen.html', \App\Action\Event\PublicEventAction::class);
-    $app->get('/videos.html', \App\Action\Movie\MovieAction::class);
 
-    $app->get('/organcms', \App\Action\Auth\LoginAction::class)->setName('organcms');
-    $app->post('/organcms', \App\Action\Auth\LoginSubmitAction::class);
+    /*
+    *----------------------------------------------------------------------------
+    * Frontend
+    *----------------------------------------------------------------------------
+    */
+
+    $app->get('/', \App\Action\Frontend\Home\HomeAction::class);
+    $app->get('/datenschutzerklaerung.html', \App\Action\Frontend\Dsgvo\DsgvoAction::class);
+    $app->get('/gallerie.html', \App\Action\Frontend\Gallery\GalleryAction::class);
+    $app->get('/impressum.html', \App\Action\Frontend\Imprint\ImprintAction::class);
+    $app->get('/blog.html', \App\Action\Frontend\Post\PostAction::class);
+    $app->get('/blog/{id}/{slug}.html', \App\Action\Frontend\Post\ReadAction::class);
+    $app->get('/orgelakte.html', \App\Action\Frontend\OrganAct\OrganActAction::class);
+    $app->get('/orgelfreunde.html', \App\Action\Frontend\OrganFriend\OrganFriendAction::class);
+    $app->get('/unsere-orgel.html', \App\Action\Frontend\OurOrgan\OurOrganAction::class);
+    $app->get('/veranstaltungen.html', \App\Action\Event\PublicEventAction::class);
+    $app->get('/videos.html', \App\Action\Frontend\Movie\MovieAction::class);
+
+    /*
+    *----------------------------------------------------------------------------
+    * Backend
+    *----------------------------------------------------------------------------
+    */
+
+    $app->get('/organcms', \App\Action\Backend\Auth\LoginAction::class)->setName('organcms');
+    $app->post('/organcms', \App\Action\Backend\Auth\LoginSubmitAction::class);
 
     $app->group('/dashboard', function (RouteCollectorProxy $group) {
-        $group->get('/', \App\Action\Dashboard\DashboardAction::class)->setName('dashboard');
+        $group->get('/', \App\Action\Backend\Dashboard\DashboardAction::class)->setName('dashboard');
     })->add(UserAuthMiddleware::class);
 
     $app->group('/donation', function (RouteCollectorProxy $group) {
-        $group->get('/', \App\Action\Donation\DonationDetailsAction::class)->setName('donation');
+        $group->get('/', \App\Action\Backend\Donation\DonationDetailsAction::class)->setName('donation');
         $group->post('/update', \App\Action\Donation\UpdateAction::class)->setName('donation');
     })->add(UserAuthMiddleware::class);
 
     $app->group('/event', function (RouteCollectorProxy $group) {
-        $group->get('/', \App\Action\Event\EventAction::class)->setName('events');
-        $group->get('/new', \App\Action\Event\NewEventAction::class)->setName('events');
-        $group->get('/read/{id}', \App\Action\Event\ReadAction::class)->setName('read-event');
-        $group->get('/delete/{id}', \App\Action\Event\DeleteAction::class);
-        $group->post('/create', \App\Action\Event\CreateAction::class);
-        $group->post('/update', \App\Action\Event\UpdateAction::class);
+        $group->get('/', \App\Action\Backend\Event\EventAction::class)->setName('events');
+        $group->get('/new', \App\Action\Backend\Event\NewEventAction::class)->setName('events');
+        $group->get('/read/{id}', \App\Action\Backend\Event\ReadAction::class)->setName('read-event');
+        $group->get('/delete/{id}', \App\Action\Backend\Event\DeleteAction::class);
+        $group->post('/create', \App\Action\Backend\Event\CreateAction::class);
+        $group->post('/update', \App\Action\Backend\Event\UpdateAction::class);
     })->add(UserAuthMiddleware::class);
 
     $app->group('/post', function (RouteCollectorProxy $group) {
-        $group->get('/', \App\Action\Post\PostAction::class)->setName('posts');
-        $group->get('/new', \App\Action\Post\NewPostAction::class);
+        $group->get('/', \App\Action\Backend\Post\PostAction::class)->setName('posts');
+        $group->get('/new', \App\Action\Backend\Post\NewPostAction::class);
         $group->post('/create', \App\Action\Post\CreateAction::class);
         $group->post('/update', \App\Action\Post\UpdateAction::class);
-        $group->get('/edit/{id}', \App\Action\Post\ReadAction::class)->setName('read-post');
+        $group->get('/edit/{id}', \App\Action\Backend\Post\ReadAction::class)->setName('read-post');
         $group->get('/delete/{id}', \App\Action\Post\DeleteAction::class)->setName('read-post');
     })->add(UserAuthMiddleware::class);
 
     $app->group('/settings', function (RouteCollectorProxy $group) {
-        $group->get('/general', \App\Action\Settings\GeneralAction::class);
+        $group->get('/general', \App\Action\Backend\Settings\GeneralAction::class);
     })->add(UserAuthMiddleware::class);
 
     $app->group('/supporter', function (RouteCollectorProxy $group) {
-        $group->get('/', \App\Action\Supporter\SupporterAction::class)->setName('supporter');
-        $group->get('/new', \App\Action\Supporter\NewSupporterAction::class);
-        $group->get('/edit/{id}', \App\Action\Supporter\ReadAction::class);
+        $group->get('/', \App\Action\Backend\Supporter\SupporterAction::class)->setName('supporter');
+        $group->get('/new', \App\Action\Backend\Supporter\NewSupporterAction::class);
+        $group->get('/edit/{id}', \App\Action\Backend\Supporter\ReadAction::class);
         $group->get('/delete/{id}', \App\Action\Supporter\DeleteAction::class);
         $group->post('/create', \App\Action\Supporter\CreateAction::class);
         $group->post('/update', \App\Action\Supporter\UpdateAction::class);
     });
 
     $app->group('/user', function (RouteCollectorProxy $group) {
-        $group->get('/user', \App\Action\User\AboutAction::class)->setName('users');
+        $group->get('/user', \App\Action\Backend\User\AboutAction::class)->setName('users');
         $group->post('/user/update', \App\Action\User\UpdateAction::class)->setName('users');
-        $group->get('/logout', \App\Action\Auth\LogoutAction::class)->setName('logout');
+        $group->get('/logout', \App\Action\Backend\Auth\LogoutAction::class)->setName('logout');
     })->add(UserAuthMiddleware::class);
 };

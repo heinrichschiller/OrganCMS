4093abf0c6623ffece15f3a05aa310e8b056b83c
heinrichschiller <heinrich@heinrich-schiller.de>
2024-04-08 09:54:01 +0200

    moved to Action/Frontend

--- src/Action/Frontend/OrganFriend/OrganFriendAction.php
+++ src/Action/Frontend/OrganFriend/OrganFriendAction.php
@@ -2,7 +2,7 @@
 
 declare (strict_types=1);
 
-namespace App\Action\OrganFriend;
+namespace App\Action\Frontend\OrganFriend;
 
 use App\Renderer\TemplateRenderer;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -37,7 +37,7 @@ final class OrganFriendAction
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $response = $this->renderer->render($response, 'friends/orgelfreunde.html', []);
+        $response = $this->renderer->render($response, 'frontend/friends/orgelfreunde.html', []);
 
         return $response;
     }

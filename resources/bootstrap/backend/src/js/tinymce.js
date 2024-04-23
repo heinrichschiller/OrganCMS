1ec390b85f732e8bb7d1a274fb121e2d644f54df
heinrichschiller <heinrich@heinrich-schiller.de>
2024-04-23 17:54:36 +0200

    changed

--- resources/bootstrap/backend/src/js/tinymce.js
+++ resources/bootstrap/backend/src/js/tinymce.js
@@ -4,7 +4,12 @@ tinymce.init({
     selector: 'textarea#default-editor',
     plugins: 'image media link',
     menubar: false,
-    toolbar: 'undo redo | bold italic underline strikethrough | fontsize  fontname | alignleft aligncenter alignright alignjustify | link | image media'
+    toolbar: 'undo redo | bold italic underline strikethrough | fontsize  fontname | alignleft aligncenter alignright alignjustify | link | image media',
+    image_advtab: true,
+    image_class_list: [
+        { title: 'None', value: '' },
+        { title: 'Default', value: 'img-fluid' }
+    ]
 })
 
 tinymce.init({

a3d0115878faa7a664d6767df3e2b971f6772611
heinrichschiller <heinrich@heinrich-schiller.de>
2024-05-01 10:43:33 +0200

    bugfix, cache deactivated due to missing PHP-APCU extension on the domain https://www.lutherorgel-plauen.de/

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -18,14 +18,19 @@ if (version_compare(phpversion(), '8.2.0', '<')) {
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 
+// @todo Der APCu Cache ist auf der https://www.lutherorgel-plauen.de/ nicht aktiviert, deshalb
+// kommt es zu einen Laufzeit-Error und der Code hier deaktiviert.
+// Wieder aktivieren wenn es dann in ein Docker-Container umgezogen ist und läuft.
+//
 // Do not compile the container in a development environment, else all the
 // changes you make to the definitions (attributes, configuration files, etc.)
 // will not be taken into account.
 // https://php-di.org/doc/performances.html#development-environment
-if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'prod'
-    || isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'prod') {
-    $containerBuilder->enableCompilation(__DIR__ . '/../var/caches/php-di');
-}
+// if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'prod'
+//    || isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'prod') {
+//    $containerBuilder->enableCompilation(__DIR__ . '/../var/caches/php-di');
+//    $containerBuilder->enableDefinitionCache();
+// }
 
 $container = $containerBuilder->build();
 

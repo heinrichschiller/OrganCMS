bb18df9ac64e15ece6853d5c19e2793aa913cf03
heinrichschiller <heinrich@heinrich-schiller.de>
2025-09-10 15:40:09 +0200

    added, OrganActActionTest

---
+++ tests/TestCase/Action/Frontend/OrganAct/OrganActActionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\TestCase\Action\Frontend\OrganAct;
+
+use App\Action\Frontend\OrganAct\OrganActAction;
+use App\Renderer\TemplateRenderer;
+use Fig\Http\Message\StatusCodeInterface;
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+#[CoversClass(OrganActAction::class)]
+#[UsesClass(TemplateRenderer::class)]
+class OrganActActionTest extends TestCase
+{
+    public function testInvokeRendersTemplateAndReturnsOk(): void
+    {
+        $renderer = $this->createMock(TemplateRenderer::class);
+        $renderer->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->isInstanceOf(ResponseInterface::class),
+                'frontend/organact/organ-act',
+                []
+            )->willReturn(new Response());
+        
+        $action = new OrganActAction($renderer);
+
+        $response = $action(new Response());
+
+        $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
+    }
+}

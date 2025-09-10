0e421bdb5daed65aba93042bab12255cdd5824c9
heinrichschiller <heinrich@heinrich-schiller.de>
2025-09-10 15:41:03 +0200

    added, HomeActionTest

--- tests/TestCase/Action/Frontend/Home/HomeActionTest.php
+++ tests/TestCase/Action/Frontend/Home/HomeActionTest.php
@@ -1,32 +1,47 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Tests\TestCase\Action\Frontend\Home;
 
+use App\Action\Frontend\Home\HomeAction;
+use App\Domain\Home\Service\HomeReader;
+use App\Renderer\TemplateRenderer;
 use Fig\Http\Message\StatusCodeInterface;
+use Nyholm\Psr7\Response;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 use Tests\Traits\AppTestTrait;
 
-#[CoversClass('\App\Action\Frontend\Home\HomeAction')]
+#[CoversClass(HomeAction::class)]
+#[UsesClass(HomeReader::class)]
+#[UsesClass(TemplateRenderer::class)]
 class HomeActionTest extends TestCase
 {
     use AppTestTrait;
 
-    // public function testAction(): void
-    // {
-    //     $request = $this->createRequest('GET', '/');
-    //     $response = $this->app->handle($request);
-
-    //     $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
-    //     $this->assertResponseContains($response, '<!DOCTYPE html>');
-    // }
-
-    public function testPageNotFound(): void
+    public function testInvokeRendersTemplateAndReturnsOk(): void
     {
-        // $request = $this->createRequest('GET', '/nada');
-        // $response = $this->app->handle($request);
+        $reader = $this->createMock(HomeReader::class);
+        $reader->expects($this->once())
+            ->method('read')
+            ->willReturn([]);
 
-        $this->assertFalse(false);
-        // $this->assertSame(StatusCodeInterface::STATUS_NOT_FOUND, $response->getStatusCode());
+        $renderer = $this->createMock(TemplateRenderer::class);
+        $renderer->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->isInstanceOf(ResponseInterface::class),
+                'frontend/home/index',
+                []
+            )->willReturn(new Response());
+
+        $action = new HomeAction($reader, $renderer);
+
+        $response = $action(new Response());
+
+        $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
     }
 }

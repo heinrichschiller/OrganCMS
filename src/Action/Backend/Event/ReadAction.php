ceefe43a0357891ee5fb459dce29a6ce0e762d7d
heinrichschiller <heinrich@heinrich-schiller.de>
2025-11-06 12:53:57 +0100

    update, ReadActionTest

--- src/Action/Backend/Event/ReadAction.php
+++ src/Action/Backend/Event/ReadAction.php
@@ -70,7 +70,7 @@ final class ReadAction
         $flash->clear();
 
         $id = (int) $request->getAttribute('id');
-        $event = $this->finder->findByIdOrFail($id);
+        $event = $this->finder->findById($id);
 
         $data = [
             'event' => $event,

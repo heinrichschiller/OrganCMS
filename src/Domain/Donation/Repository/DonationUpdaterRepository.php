87bf083cd377c1d1465f78441defc676f9b8198c
heinrichschiller <heinrich@heinrich-schiller.de>
2025-12-02 13:47:36 +0100

    fix, namespace

--- src/Domain/Donation/Repository/DonationUpdaterRepository.php
+++ src/Domain/Donation/Repository/DonationUpdaterRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Donation\Repository;
 
-use App\Domain\Donation\DonationDetails;
+use App\Domain\Donation\Data\DonationDetails;
 use Doctrine\DBAL\Connection;
 
 final class DonationUpdaterRepository
